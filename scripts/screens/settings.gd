dbde5b68ba0043b7994e0fcadee49c28e606f852
Joe Vogel <joevogel501@gmail.com>
2025-10-26 19:53:27 -0700

    Add simple pause menu
    'Esc' opens up a pause menu and stops all world processes

--- scripts/screens/settings.gd
+++ scripts/screens/settings.gd
@@ -1,1 +1,10 @@
-extends Node2D
+extends Control
+
+func _ready():
+	get_parent().visible = false
+
+func _input(event):
+	if event.is_action_pressed("settings"):
+		get_tree().paused = !get_tree().paused
+		get_parent().visible = get_tree().paused
+		print(get_parent())
