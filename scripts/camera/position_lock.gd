17c8e6c87a97597ea426d906118ae4ba532d1fbf
BuilderRobot <jake@stellyfamily.com>
2025-10-31 16:45:05 -0700

    player camera

---
+++ scripts/camera/position_lock.gd
@@ -0,0 +1,18 @@
+class_name PositionLock
+extends Camera2D
+
+
+@export var player:CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var ppos:Vector2 = player.global_position
+	
+	global_position = ppos
