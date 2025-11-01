b69870584e2432dc04f964c9223e8bb31dcee220
wk-y <86206045+wk-y@users.noreply.github.com>
2025-11-01 15:40:25 -0700

    Resource Engine

--- scripts/managers/resource_manager.gd
+++ scripts/managers/resource_manager.gd
@@ -1,5 +1,36 @@
 class_name ResourceManager
 extends Node
 
+enum ResourceType {
+	FOOD,
+	ELECTRICITY,
+	IRON,
+	POPULATION,
+}
+
+var resources:Dictionary[ResourceType, float]
+var engines:Dictionary[ResourceType, ResourceEngine]
+
+
 func _ready() -> void:
-	pass
+	resources = {
+		ResourceType.FOOD: 100,
+		ResourceType.ELECTRICITY: 100,
+		ResourceType.IRON: 100,
+		ResourceType.POPULATION: 100,
+	}
+	
+	for type in ResourceType:
+		assert(type in resources)
+	
+	for type in ResourceType:
+		engines[type] = ResourceEngine.new()
+
+
+## Calculates gain/loss value with modifiers applied
+##
+## Does not update the value of the resource.
+func calculate_modifiers(actor:Node, value:float, resource_type:ResourceType) -> float:
+	return engines[resource_type].apply(actor, value)
+	
+	
