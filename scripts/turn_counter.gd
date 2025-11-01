518cd6775b6cdc81cd807b1cbac130dc83bce636
wk-y <86206045+wk-y@users.noreply.github.com>
2025-11-01 16:02:00 -0700

    Turn counter

---
+++ scripts/turn_counter.gd
@@ -0,0 +1,11 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Signals.turn_started.connect(_update_turn_number)
+	_update_turn_number(game_manager.turn)
+
+
+func _update_turn_number(turn:int) -> void:
+	text = "Turn %d" % turn
