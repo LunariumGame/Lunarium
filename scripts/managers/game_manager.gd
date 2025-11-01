0e96e6de3a4d844ad192b2a10bc4559880a8c16a
wk-y <86206045+wk-y@users.noreply.github.com>
2025-11-01 15:53:30 -0700

    Game Manager Stub

--- scripts/managers/game_manager.gd
+++ scripts/managers/game_manager.gd
@@ -1,5 +1,16 @@
 class_name GameManager
 extends Node
 
+var turn:int = 1
+
+
 func _ready() -> void:
 	pass
+
+
+func end_turn() -> void:
+	print("Ending turn ", turn)
+	Signals.turn_ended.emit(turn)
+	turn += 1
+	print("Starting turn ", turn)
+	Signals.turn_started.emit(turn)
