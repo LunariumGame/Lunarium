6762d0c670f918c0d88312397a1bc147b1d9d104
wk-y <86206045+wk-y@users.noreply.github.com>
2025-11-02 15:33:00 -0800

    Basic resource display

---
+++ scripts/ui_components/resource_display.gd
@@ -0,0 +1,24 @@
+extends Label
+
+@export var resource:ResourceManager.ResourceType:
+	set(v):
+		resource = v
+		if is_node_ready():
+			_update_display(resource_manager.get_resource(resource))
+	get:
+		return resource
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Signals.resource_value_changed.connect(_on_resource_value_changed)
+	_update_display(resource_manager.get_resource(resource))
+
+func _on_resource_value_changed(_resource:ResourceManager.ResourceType, value:float) -> void:
+	if _resource == resource:
+		_update_display(value)
+
+
+func _update_display(value:float) -> void:
+	var resource_string: String = ResourceManager.ResourceType.keys()[resource]
+	text = "%s: %d" % [resource_string.to_pascal_case(), value]
