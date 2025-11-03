d29f1700e715dba4ba3b0548006e027e4d39c521
wk-y <86206045+wk-y@users.noreply.github.com>
2025-11-02 16:11:56 -0800

    Implement iron refinery logic

--- scripts/buildings/iron_refinery.gd
+++ scripts/buildings/iron_refinery.gd
@@ -1,1 +1,19 @@
-extends Node2D
+class_name IronRefinery
+extends Building
+
+const PRODUCED_RESOURCE = ResourceManager.ResourceType.IRON
+
+var level = 1
+
+
+func _ready() -> void:
+	Signals.turn_started.connect(_on_turn_started)
+
+
+func _on_turn_started(_turn:int) -> void:
+	resource_manager.calculate_and_update(PRODUCED_RESOURCE, self, _production_at_level(level))
+	print(resource_manager.get_resource(PRODUCED_RESOURCE))
+
+
+static func _production_at_level(_level:int) -> float:
+	return _level * 4
