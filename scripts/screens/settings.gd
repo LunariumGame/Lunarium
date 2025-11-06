de5e1502e8ca7c1157b21d00bce98703eef9099b
Set Paing <setpaing20022002@gmail.com>
2025-11-05 17:04:26 -0800

    window manager and UI Stack

--- scripts/screens/settings.gd
+++ scripts/screens/settings.gd
@@ -1,4 +1,4 @@
-extends Control
+extends CanvasLayer
 
 @export var alpha_speed: float = 0.75
 @export var min_alpha: float = 0.3
@@ -6,18 +6,23 @@ extends Control
 
 var _time := 0.0
 
-@onready var rotating_moon: AnimatedSprite2D = $RotatingMoon
-@onready var title: Sprite2D = $LunariumTitle
-@onready var master_volume: HSlider = $"AudioSettings/MasterVolume"
-@onready var music_volume: HSlider = $"AudioSettings/MusicVolume"
-@onready var effects_volume: HSlider = $"AudioSettings/EffectsVolume"
-
+@onready var rotating_moon: AnimatedSprite2D = $Settings/RotatingMoon
+@onready var title: Sprite2D = $Settings/LunariumTitle
+@onready var master_volume: HSlider = $"Settings/AudioSettings/MasterVolume"
+@onready var music_volume: HSlider = $"Settings/AudioSettings/MusicVolume"
+@onready var effects_volume: HSlider = $"Settings/AudioSettings/EffectsVolume"
 
 func _ready():
-	get_parent().visible = false
-	master_volume.value = 0.5
-	music_volume.value = 0.5
-	effects_volume.value = 0.5
+	master_volume.value = settings_data.volume_value_master
+	music_volume.value = settings_data.volume_value_music
+	effects_volume.value = settings_data.volume_value_effects
+
+	get_tree().paused = true
+
+func close():
+	get_tree().paused = false
+	window_manager.pop()
+	queue_free()
 
 func _process(delta):
 	#region Fluctuate alpha of title and rotating moon
@@ -29,19 +34,26 @@ func _process(delta):
 	#endregion
 
 func _input(event):
-	if event.is_action_pressed("settings"):
-		get_tree().paused = !get_tree().paused
-		get_parent().visible = get_tree().paused
-		print(get_parent())
+	if event.is_action_pressed("ui_cancel"):
+		close()
+		get_viewport().set_input_as_handled()
+	
+	#if event.is_action_pressed("settings"):
+		#get_tree().paused = !get_tree().paused
+		#get_parent().visible = get_tree().paused
+		#print(get_parent())
 
 
 func _on_master_volume_value_changed(value: float) -> void:
+	settings_data.volume_value_master = value
 	AudioServer.set_bus_volume_db(0, linear_to_db(value))
 
 
 func _on_music_volume_value_changed(value: float) -> void:
+	settings_data.volume_value_music = value
 	AudioServer.set_bus_volume_db(1, linear_to_db(value))
 
 
 func _on_effects_volume_value_changed(value: float) -> void:
+	settings_data.volume_value_effects = value
 	AudioServer.set_bus_volume_db(2, linear_to_db(value))
