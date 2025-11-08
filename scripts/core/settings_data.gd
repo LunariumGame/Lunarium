d5a4b01698d156936f7dc0da648b31e5add61721
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2025-11-08 14:33:18 -0800

    normalize in-game audio for settings menu

--- scripts/core/settings_data.gd
+++ scripts/core/settings_data.gd
@@ -1,5 +1,5 @@
 extends Node
 
-var volume_value_master: float = 0.5
-var volume_value_music: float = 0.5
-var volume_value_effects: float = 0.5
+var volume_value_master: float = 1.0
+var volume_value_music: float = 1.0
+var volume_value_effects: float = 1.0
