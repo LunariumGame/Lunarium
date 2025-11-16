4061f3e99f5566a7b150dbd0a64a8021ee772a0b
Wen Kai Yiang <wyiang@ucdavis.edu>
2025-11-16 15:14:24 -0800

    Implement basics of electricity usage by buildings

--- scripts/buildings/building.gd
+++ scripts/buildings/building.gd
@@ -1,2 +1,31 @@
+@abstract
 class_name Building
 extends Node2D
+
+var is_powered:bool
+
+
+func _ready() -> void:
+	Signals.turn_started.connect(_on_turn_started)
+	Signals.turn_ended.connect(_on_turn_ended)
+
+
+func get_power_draw() -> float:
+	return 0
+
+
+## Overriding implementations should call super() at the beginning .
+func _on_turn_started(_turn_number:int) -> void:
+	var power_draw:float = get_power_draw()
+	var available_electricity:float = resource_manager.get_resource(
+			ResourceManager.ResourceType.ELECTRICITY)
+	
+	is_powered = power_draw <= available_electricity
+	if is_powered:
+		resource_manager.add_precalculated(
+				ResourceManager.ResourceType.ELECTRICITY, -power_draw)
+
+
+## Overriding implementations should call super() at the beginning .
+func _on_turn_ended(_turn_number:int) -> void:
+	pass
