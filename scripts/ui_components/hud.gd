2edeac96db6609dcc84affd838fa9ec82b135226
Joe Vogel <joevogel501@gmail.com>
2025-11-16 14:27:21 -0800

    Add 3s cooldown to next turn button, with progress bar

--- scripts/ui_components/hud.gd
+++ scripts/ui_components/hud.gd
@@ -1,6 +1,8 @@
 class_name HUD
 extends CanvasLayer
 
+@onready var next_turn_button:Button = %NextTurn
+
 
 func _on_settings_pressed() -> void:
 	Signals.settings_opened.emit()
@@ -8,3 +10,4 @@ func _on_settings_pressed() -> void:
 
 func _on_next_turn_pressed() -> void:
 	game_manager.end_turn()
+	next_turn_button.start_cooldown()
---
+++ scripts/ui_components/next_turn.gd
@@ -0,0 +1,30 @@
+extends Button
+
+@export var cooldown_time: float = 3.0 # seconds
+
+var cooldown_active: bool = false
+
+@onready var progress_bar: ProgressBar = $ProgressBar
+
+
+func _ready():
+	progress_bar.visible = false
+	progress_bar.value = 0
+
+
+func start_cooldown():
+	cooldown_active = true
+	progress_bar.visible = true
+	progress_bar.modulate = Color(1,1,1,0.5)
+	disabled = true
+
+	var tween = get_tree().create_tween()
+	tween.tween_property(progress_bar, "value", progress_bar.max_value, cooldown_time)
+	tween.tween_callback(Callable(self, "_end_cooldown"))
+
+func _end_cooldown():
+	disabled = false
+	progress_bar.value = 0
+	progress_bar.visible = false
+	cooldown_active = false
+	
