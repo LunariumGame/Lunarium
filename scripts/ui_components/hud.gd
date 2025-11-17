fff4728454d15b28bd3150b8eee073523a982d51
Joe Vogel <joevogel501@gmail.com>
2025-11-16 20:14:20 -0800

    Implement 'inspector panel'
    Like AoE2 @Hawkhobo

--- scripts/ui_components/hud.gd
+++ scripts/ui_components/hud.gd
@@ -1,8 +1,19 @@
 class_name HUD
 extends CanvasLayer
 
-@onready var next_turn_button:Button = %NextTurn
+enum Systems {TECH, BUILDING, BOT}
 
+@onready var next_turn_button:Button = %NextTurn
+@onready var system_buttons: Array[Button] = [
+	%SystemButtons/TechTree,
+	%SystemButtons/BuildingManager,
+	%SystemButtons/BotManager
+]
+@onready var system_panels: Array[PanelContainer] = [
+	%InspectorPanel/TechInspector,
+	%InspectorPanel/BuildingInspector,
+	%InspectorPanel/BotInspector
+]
 
 func _on_settings_pressed() -> void:
 	Signals.settings_opened.emit()
@@ -13,13 +24,34 @@ func _on_next_turn_pressed() -> void:
 	next_turn_button.start_cooldown()
 
 
+#region System Buttons\
+func toggle_panel(system: Systems) -> void:
+	# Toggle off if already toggled
+	if system_panels[system].visible == true:
+		system_panels[system].visible = false;
+		system_buttons[system].button_pressed = false;
+		return
+	
+	# Toggle the rest off and enable selected
+	for i in Systems.values():  # Loop over TECH, BUILDING, BOT
+		var is_selected:bool = (i == system)
+		system_buttons[i].button_pressed = is_selected
+		system_panels[i].visible = is_selected
+
+
 func _on_tech_tree_pressed() -> void:
-	pass # Replace with function body.
+	# Untoggle other buttons
+	toggle_panel(Systems.TECH)
 
 
 func _on_building_manager_pressed() -> void:
-	pass # Replace with function body.
+	# Untoggle other buttons
+	toggle_panel(Systems.BUILDING)
 
 
 func _on_bot_manager_pressed() -> void:
-	pass # Replace with function body.
+	# Untoggle other buttons
+	toggle_panel(Systems.BOT)
+
+
+#endregion
