7b98248cc1cb4a6bc2249e1753cd31ba2f1e832b
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2025-11-18 21:52:21 -0800

    player can place as many buildings as they want; no restrictions yet

--- scripts/ui_components/building/building_cursor.gd
+++ scripts/ui_components/building/building_cursor.gd
@@ -40,8 +40,13 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _place_building() -> void:
 	var building_instance = building_scene.instantiate()
-	
+
 	if (building_instance == null):
 		push_error("<building_cursor.gd, L45> building was not initialized prior to instantiation")
-		
-	
+
+	building_instance.global_position = get_global_mouse_position()
+
+	var colony_buildings_node = get_tree().get_root().get_node("World/PlacedBuildings")
+	colony_buildings_node.add_child(building_instance)
+
+	print("successfully added to colony: ", building_instance.name)
