8b021dc221667b608d55d22e5b6c18c863ba5335
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2025-11-20 11:02:35 -0800

    reorganize active button tracker

--- scripts/ui_components/building/active_button_tracker.gd
+++ scripts/ui_components/building/active_button_tracker.gd
@@ -7,24 +7,26 @@ func _ready() -> void:
 
 # only one active button at a time	
 func button_activated(active_button: BuildingButton) -> void:
-	# check if active_button is already active
-	var existing_cursor: BuildingCursor = ( 
-		active_button.get_node_or_null(^"BuildingCursor")
-	)
-	if existing_cursor and is_instance_valid(existing_cursor):
-		existing_cursor.queue_free()
-		active_button.building_instance = null
+	# check if active_button is already active	
+	if _free_cursor(active_button):
 		return
 
 	# for all non-active buttons with cursor, clear to prioritize active button		
 	for button in get_tree().get_nodes_in_group("building_buttons"):
 		if button != active_button:
-			var node_to_free: BuildingCursor = ( 
-				button.get_node_or_null(^"BuildingCursor")
-			)
+			_free_cursor(button)
 
-			# is_instance_valid prevents double free
-			if node_to_free and is_instance_valid(node_to_free):
-				print("freeing ", node_to_free, " from ", button)
-				node_to_free.queue_free()
-				button.building_instance = null
+
+func _free_cursor(target_button: BuildingButton) -> bool:
+	var node_to_free: BuildingCursor = ( 
+		target_button.get_node_or_null(^"BuildingCursor")
+	)
+
+	# is_instance_valid prevents double free
+	if node_to_free and is_instance_valid(node_to_free):
+		print("freeing ", node_to_free, " from ", target_button)
+		node_to_free.queue_free()
+		target_button.building_instance = null
+		return true
+	
+	return false
