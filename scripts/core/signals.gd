778f1a62a9fe5b118c34cd4725342890ff64d6db
Wen Kai Yiang <wyiang@ucdavis.edu>
2025-11-20 17:42:11 -0800

    Add a new phase for electricity generation

--- scripts/core/signals.gd
+++ scripts/core/signals.gd
@@ -6,6 +6,9 @@ signal turn_started(turn_number:int)
 ## Fires at the end of a turn.
 signal turn_ended(turn_number:int)
 
+## Fires during the electricity generation phase of the inter-turn period
+signal turn_electricity_generation(previous_turn_number:int)
+
 
 ## Emitted when the amount of a resource that the player has is changed.
 signal resource_value_changed(resource:ResourceManager.ResourceType, value:float)
--- scripts/managers/game_manager.gd
+++ scripts/managers/game_manager.gd
@@ -11,6 +11,11 @@ func _ready() -> void:
 func end_turn() -> void:
 	print("Ending turn ", turn)
 	Signals.turn_ended.emit(turn)
+	
+	# Handle electricity calculations
+	resource_manager.set_resource(ResourceManager.ResourceType.ELECTRICITY, 0)
+	Signals.turn_electricity_generation.emit(turn)
+	
 	turn += 1
 	print("Starting turn ", turn)
 	Signals.turn_started.emit(turn)
