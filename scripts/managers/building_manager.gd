dfb4783bd50196c646e365376f713e09fb0841e5
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2025-12-03 15:54:20 -0800

    remove iron refinery in world

--- scripts/managers/building_manager.gd
+++ scripts/managers/building_manager.gd
@@ -5,14 +5,10 @@ const WIDTH: int = 10
 const HEIGHT: int = 10
 
 const DIRECTIONS = [
-	Vector2i(1, 0), 
-	Vector2i(-1, 0),
-	Vector2i(0, 1), 
-	Vector2i(0, -1),
-	Vector2i(1, 1), 
-	Vector2i(-1, 1),
-	Vector2i(1, -1), 
-	Vector2i(-1, -1),
+	Vector2i(1, 0), Vector2i(-1, 0),
+	Vector2i(0, 1), Vector2i(0, -1),
+	Vector2i(1, 1), Vector2i(-1, 1),
+	Vector2i(1, -1), Vector2i(-1, -1),
 ]
 
 enum BuildingType {
