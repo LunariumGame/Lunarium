a4d04718904abaf51005f54e4d2edfa88f7d9ad8
Wen Kai Yiang <wyiang@ucdavis.edu>
2025-12-02 22:13:59 -0800

    Add starvation mechanic

--- scripts/managers/game_manager.gd
+++ scripts/managers/game_manager.gd
@@ -11,9 +11,13 @@ enum GameState {
 
 const WIN_CONDITION_MIN_POPULATION:int = 500
 
+const COLONIST_CONSUMPTION_PER_TURN:float = 1
+const STARVING_COLONIST_DEATH_RATE_PER_TURN:float = 0.5
+
 var turn:int = 1
 
 var state := GameState.IN_PROGRESS
+
 
 func _ready() -> void:
 	pass
@@ -29,6 +33,8 @@ func end_turn() -> void:
 	# Handle electricity calculations
 	resource_manager.set_resource(ResourceManager.ResourceType.ELECTRICITY, 0)
 	Signals.turn_electricity_generation.emit(turn)
+	
+	_logic_food_consumption_and_starvation()
 	
 	if _win_condition_satisfied():
 		state = GameState.WON
@@ -46,3 +52,18 @@ func _win_condition_satisfied() -> bool:
 	
 	return population > WIN_CONDITION_MIN_POPULATION;
 	
+
+func _logic_food_consumption_and_starvation() -> void:
+	# feed as many colonists as possible
+	var food:float = resource_manager.get_resource(ResourceManager.ResourceType.FOOD)
+	var population:int = roundi(resource_manager.get_resource(ResourceManager.ResourceType.POPULATION))
+	var fed_colonists:int = mini(population, floori(food / COLONIST_CONSUMPTION_PER_TURN))
+	
+	# apply starvation
+	var starving_colonists:int = population - fed_colonists
+	var deaths:int = ceili(starving_colonists * STARVING_COLONIST_DEATH_RATE_PER_TURN)
+	
+	# apply calculated effects
+	resource_manager.add_precalculated(ResourceManager.ResourceType.FOOD, -COLONIST_CONSUMPTION_PER_TURN * fed_colonists)
+	if deaths > 0:
+		resource_manager.add_precalculated(ResourceManager.ResourceType.POPULATION, -deaths)
