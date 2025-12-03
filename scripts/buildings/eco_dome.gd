e35b5c4ace687d1b8de98abd03461e4978b5d131
Wen Kai Yiang <wyiang@ucdavis.edu>
2025-12-03 15:18:35 -0800

    EcoDome production

--- scripts/buildings/eco_dome.gd
+++ scripts/buildings/eco_dome.gd
@@ -1,2 +1,12 @@
 class_name EcoDome
 extends Building
+
+const BASE_FOOD_PRODUCTION:int = 10
+
+func _on_turn_started(_turn_number:int) -> void:
+	resource_manager.calculate_and_update(
+		ResourceManager.ResourceType.FOOD,
+		self,
+		BASE_FOOD_PRODUCTION,
+		ResourceEngine.ApplyTime.ON_TURN_STARTED,
+	)
