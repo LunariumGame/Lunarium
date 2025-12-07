2f8172a911a73845f7cd1896b6cef81bb7fb0129
Joe Vogel <joevogel501@gmail.com>
2025-12-06 23:11:38 -0800

    Add camera sprint feature

--- scripts/core/player.gd
+++ scripts/core/player.gd
@@ -1,19 +1,21 @@
 class_name Player
 extends CharacterBody2D
 
-
-const JUMP_VELOCITY:float = -400.0
-
-var speed:float = 300.0
-
+@export var default_speed:float = 300
+var sprint_speed:float = default_speed * 3
+var speed:float = default_speed
 
 func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	speed = default_speed
 	var direction_horizontal := Input.get_axis("ui_left", "ui_right")
 	var direction_vertical := Input.get_axis("ui_up", "ui_down")
 	
 	var move_direction := Vector2(direction_horizontal, direction_vertical).normalized()
+	
+	if Input.is_action_pressed("sprint"):
+		speed = sprint_speed
 	
 	if direction_horizontal:
 		velocity.x = move_direction.x * speed
