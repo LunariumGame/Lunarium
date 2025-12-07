f07b6096d79d01bc15a5336f97753e9abac2b108
Wen Kai Yiang <wyiang@ucdavis.edu>
2025-12-06 16:24:07 -0800

    Update crater generation

--- scripts/craters.gd
+++ scripts/craters.gd
@@ -3,10 +3,14 @@ extends Node2D
 
 const CRATER = preload("uid://d23uxg6wsi6f3")
 
+## Scale for the offset of crater placement
+const BASE_OFFSET_SCALE = 4
+const MIN_GAP = 128
+
 @export_tool_button("Regenerate", "Callable") var regenerate = _regenerate_craters
 
-@export var crater_count:int = 500
-@export var spread:int = 10000
+@export var crater_count:int = 5000
+@export var spread:int = 100
 
 func _regenerate_craters():
 	var rng := RandomNumberGenerator.new()
@@ -14,12 +18,27 @@ func _regenerate_craters():
 	for child in get_children():
 		child.queue_free()
 	
+	var crater_poses: Array[Vector2] = []
 	for i in crater_count:
 		var crater:Sprite2D = CRATER.instantiate()
 		crater.variant = rng.rand_weighted([.4, .4, .2])
+		
+		var p:Vector2
+		while true:
+			p = Vector2(
+				rng.randi_range(-spread, spread),
+				rng.randi_range(-spread, spread),
+			) * BASE_OFFSET_SCALE
+			
+			var ok:bool = true
+			for pos in crater_poses:
+				if pos.distance_squared_to(p) < MIN_GAP * MIN_GAP:
+					ok = false
+					break
+			if ok:
+				break
+		
 		self.add_child(crater, true)
 		crater.owner = self
-		crater.position = Vector2(
-			rng.randi_range(-spread, spread),
-			rng.randi_range(-spread, spread),
-		)
+		crater.position = p
+		crater_poses.push_back(p)
