8f5c033f41ccdf00759fb1ee0cbe8434e108c4ba
Joe Vogel <joevogel501@gmail.com>
2025-12-07 14:01:22 -0800

    Adjust population to start at " / 1"

--- scripts/ui_components/resource_display.gd
+++ scripts/ui_components/resource_display.gd
@@ -42,7 +42,7 @@ func _update_display() -> void:
 	# Special-case: POPULATION uses occupied/housing capacity
 	if resource == ResourceManager.ResourceType.POPULATION:
 		var housing_cap := game_manager.get_total_housing_capacity()
-		text = "%d / %d" % [value, housing_cap]
+		text = "%d / %d" % [value, housing_cap + 1] # plus 1 for starting colonist
 		return
 
 	# If the resource has no capacity, show only the value
