5ff6a6d70f03b72ab459b8d2a8a28afbb53a5f33
Wen Kai Yiang <wyiang@ucdavis.edu>
2025-12-07 00:29:15 -0800

    Shake to find cursor

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -4,18 +4,33 @@ const BASE_WIDTH = 1920
 const BASE_HEIGHT = 1080
 const NATIVE_CURSOR_SIZE = Vector2i(32, 32)
 const CURSOR_TARGET_OFFSET = Vector2(4, 4)
+
+# safe hardware cursor size limit
 const MAX_SIZE = Vector2i(128, 128)
 
 @onready var texture_rect: TextureRect = $TextureRect
 
+var shake_amount:float = 1.0
 
 func _process(delta:float) -> void:
-	var size:Vector2i = DisplayServer.screen_get_size()
-	var scale_factor:int = mini(size.x / BASE_WIDTH, size.y / BASE_HEIGHT)
-	var max_scale:int = mini(MAX_SIZE.x / NATIVE_CURSOR_SIZE.x , MAX_SIZE.y / NATIVE_CURSOR_SIZE.y)
-	scale_factor = maxi(1, mini(scale_factor, max_scale))
+	var screen_size:Vector2i = DisplayServer.screen_get_size()
+	var max_scale:float = minf(MAX_SIZE.x * 1.0 / NATIVE_CURSOR_SIZE.x , MAX_SIZE.y * 1.0 / NATIVE_CURSOR_SIZE.y)
 	
+	# calculate cursor shake scaling
+	shake_amount += Input.get_last_mouse_velocity().length() / screen_size.length() / 20
+	shake_amount = clampf(shake_amount - delta, 0, max_scale)
+	var shake_scale:float = max(shake_amount, 1)
+	
+	# calculate cursor scale
+	var scale_factor:float = minf(1.0 * screen_size.x / BASE_WIDTH, 1.0 * screen_size.y / BASE_HEIGHT) * shake_scale
+	scale_factor = maxf(1, minf(scale_factor, max_scale))
+	
+	# apply cursor scaling
 	size = NATIVE_CURSOR_SIZE * scale_factor
 	texture_rect.size = size
+	
+	# wait for render to complete
 	await RenderingServer.frame_post_draw
+	
+	# update cursor
 	Input.set_custom_mouse_cursor(self.get_texture(), Input.CURSOR_ARROW, CURSOR_TARGET_OFFSET * scale_factor)
