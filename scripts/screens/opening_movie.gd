ba841e85aebcd4c852c9cf10186762c350c99faf
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2025-12-08 12:02:05 -0800

    add intro score, handle Main Music to loop instead of autoplay

--- scripts/screens/opening_movie.gd
+++ scripts/screens/opening_movie.gd
@@ -1,18 +1,26 @@
 extends CanvasLayer
 
 @onready var movie := $CBoxContainer/VideoStreamPlayer
+@onready var score := $IntroScore
 
 func _ready():
 	# wait some time for the opening screen to go away
 	await get_tree().create_timer(2.0).timeout
 	
-	# play the movie
+	# play the movie & audio
 	movie.play()
+	score.play()
 
 
 func _on_movie_end():
 	# signal that movie has ended
 	Signals.opening_movie_stopped.emit()
+	score.stop()
+	
+	# play main game music
+	var main_music := get_node("/root/World/Audio/Music")
+	if main_music:
+		main_music.on_finished()
 	close()
 
 
