67b8d6c560750c3f148df886ab2975e360fceeaa
Kap1la <56204067+Kap1la@users.noreply.github.com>
2025-12-08 09:21:27 -0800

    Added fade in effect on instantiation for victory/defeat screens

--- scripts/screens/victory.gd
+++ scripts/screens/victory.gd
@@ -1,5 +1,18 @@
 extends CanvasLayer
 
+@onready var sprite := $AnimatedSprite2D
+
+func _ready():
+	# Make sprite invisible at first
+	sprite.modulate.a = 0.0
+	
+	# Start playing the idle animation
+	sprite.play("idle")
+	
+	# Fade in over 1 second
+	var tween = create_tween()
+	tween.tween_property(sprite, "modulate:a", 1.0, 1.0)
+
 func close():
 	window_manager.pop()
 	queue_free()
