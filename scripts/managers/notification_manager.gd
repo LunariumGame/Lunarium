51f2bd98239ff743f45437a24d8ddc1c66bab10a
Wen Kai Yiang <wyiang@ucdavis.edu>
2025-12-08 10:25:43 -0800

    Notification manager

---
+++ scripts/managers/notification_manager.gd
@@ -0,0 +1,36 @@
+class_name NotificationManager
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Signals.colonist_died.connect(_on_colonist_died)
+	Signals.shuttle_arrived.connect(_on_shuttle_arrived)
+
+
+func _on_colonist_died(num_dead:int) -> void:
+	Signals.notification.emit(Notification.new(
+		"%d colonists have perished from starvation!" % num_dead,
+	))
+
+
+func _on_shuttle_arrived(pax:int) -> void:
+	Signals.notification.emit(Notification.new(
+		"%d colonists arrived on a shuttle" % pax ,
+	))
+
+
+func _on_shuttle_blocked_by_population_cap() -> void:
+	Signals.notification.emit(Notification.new(
+		"%d population limit reached, you must construct additional residences!"
+	))
+
+
+class Notification:
+	var text:String
+	var icon:Texture2D
+	
+	
+	func _init(_text:String, _icon:Texture2D = null) -> void:
+		text = _text
+		icon = _icon
