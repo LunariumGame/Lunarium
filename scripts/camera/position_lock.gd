dea8ec0f8383a01a5e8cec3899d951c391bc7206
Joe Vogel <joevogel501@gmail.com>
2025-12-16 20:33:03 -0800

    Camera shake on building placement

--- scripts/camera/position_lock.gd
+++ scripts/camera/position_lock.gd
@@ -6,10 +6,16 @@ extends Camera2D
 @export var zoom_speed: float = 0.1
 @export var min_zoom: float = 0.3
 @export var max_zoom: float = 5.0
+@export var set_max_shake: float = 5.0
+@export var shake_fade: float = 5.0
+
+var _curr_shake_strength: float = 0.0
 var drag_offset := Vector2.ZERO
+var max_shake: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	max_shake = set_max_shake
 	pass # Replace with function body.
 
 
@@ -18,6 +24,22 @@ func _process(_delta: float) -> void:
 	var ppos:Vector2 = player.global_position
 	
 	global_position = ppos + drag_offset
+	
+	if _curr_shake_strength > 0:
+		_curr_shake_strength = lerp(_curr_shake_strength, 0.0, shake_fade * _delta)
+		offset = Vector2(randf_range(-_curr_shake_strength, _curr_shake_strength), randf_range(-_curr_shake_strength, _curr_shake_strength))
+
+
+func trigger_shake() -> void:
+	_curr_shake_strength = max_shake
+
+
+func toggle_camera_shake() -> void:
+	# enable
+	if max_shake > 0:
+		max_shake = set_max_shake
+	else: # disable
+		max_shake = 0
 
 
 # Camera zoom and click-drag
