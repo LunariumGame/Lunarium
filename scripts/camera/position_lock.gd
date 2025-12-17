a50e122dd00fad3e4b8832802251da8e0eddbf46
Joe Vogel <joevogel501@gmail.com>
2025-12-16 18:41:13 -0800

    Drag camera feature

--- scripts/camera/position_lock.gd
+++ scripts/camera/position_lock.gd
@@ -6,6 +6,7 @@ extends Camera2D
 @export var zoom_speed: float = 0.1
 @export var min_zoom: float = 0.3
 @export var max_zoom: float = 5.0
+var drag_offset := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,10 +17,12 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	var ppos:Vector2 = player.global_position
 	
-	global_position = ppos
+	global_position = ppos + drag_offset
 
-# Camera zoom
+
+# Camera zoom and click-drag
 func _unhandled_input(event):
+	# Zoom camera
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			zoom -= Vector2(zoom_speed, zoom_speed)
@@ -29,3 +32,8 @@ func _unhandled_input(event):
 		# clamp so zoom never becomes too large or tiny
 		zoom.x = clamp(zoom.x, min_zoom, max_zoom)
 		zoom.y = clamp(zoom.y, min_zoom, max_zoom)
+
+	# Click and drag camera
+	if event is InputEventMouseMotion:
+		if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
+			drag_offset -= event.relative / zoom
