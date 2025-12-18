9966db7a34f918805e205ed22aba147e3a3e6c33
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2025-12-18 02:08:31 -0800

    credits fade in over a couple seconds. click audio for credits button

--- scripts/screens/credits.gd
+++ scripts/screens/credits.gd
@@ -1,9 +1,16 @@
 extends CanvasLayer
 
+@onready var credits := $Control
+
 
 func _ready() -> void:
 	print("credits called")
-	visible = true
+	
+	# Make credits invisible at first
+	credits.modulate.a = 0.0
+	# Fade in over 1 second
+	var tween = create_tween()
+	tween.tween_property(credits, "modulate:a", 1.0, 3.0)
 
 
 # called when "continue" button is clicked
